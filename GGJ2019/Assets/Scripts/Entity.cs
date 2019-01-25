efc1ff2a7cc7f24f236d9e0784edf4fc1cb02edc
Vojtěch Matouš <vojtim11@seznam.cz>
2019-01-26 00:36:07 +0100

    Entities scripts
    + tilemap placeholders

---
+++ GGJ2019/Assets/Scripts/Entity.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Entity : MonoBehaviour
+{
+    [SerializeField]
+    float hp;
+    [SerializeField]
+    int damage;
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float jumpSpeed;
+
+    public Rigidbody2D rigidbody2;
+    
+    void Start()
+    {
+        rigidbody2 = GetComponent<Rigidbody2D>();
+    }
+
+    void TakeDamage(int amount)
+    {
+        hp -= amount;
+
+        if (hp <= 0)
+            Die();
+    }
+
+    public virtual void Die()
+    {
+
+    }
+
+    public void Move(Vector2 direction)
+    {
+        Vector2 movementVector = new Vector2(Mathf.Clamp((direction.x * speed)+rigidbody2.velocity.x, -speed, speed), (direction.y * jumpSpeed)+rigidbody2.velocity.y);
+
+        rigidbody2.velocity = movementVector;
+    }
+}
