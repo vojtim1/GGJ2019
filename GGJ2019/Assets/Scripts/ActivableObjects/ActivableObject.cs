4eb08da5cd99d7cda872c57a2a2714e880d457f3
Vojtěch Matouš <vojtim11@seznam.cz>
2019-01-26 10:25:40 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ GGJ2019/Assets/Scripts/ActivableObjects/ActivableObject.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivableObject : MonoBehaviour
+{
+    [SerializeField]
+    bool toggleSelf = false; //E.G. change sprite on toggle
+    [SerializeField]
+    bool isActivator = false; //Can be activated by pressing "E" directly or by some other object?
+    [SerializeField]
+    protected bool isActivated = false;
+    [SerializeField]
+    List<GameObject> gameObjectsToToggle;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (isActivator)
+            {
+                print("Press 'E' to pull the lever.");
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (isActivator)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    //print("Activating");
+                    Toggle();
+                }
+            }
+        }
+    }
+
+    void Toggle()
+    {
+        if (toggleSelf)
+            OnToggle();
+        foreach (GameObject go in gameObjectsToToggle)
+        {
+            go.SendMessage("OnToggle");
+        }
+    }
+
+    public virtual void OnToggle()
+    {
+        print("toggling");
+        isActivated = !isActivated;
+    }
+}
---
+++ GGJ2019/Assets/Scripts/ActivableObjects/SpriteChangingObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SpriteChangingObject : ActivableObject
+{
+    [Header("Sprite changing")]
+    [SerializeField]
+    Sprite defaultSprite;
+    [SerializeField]
+    Sprite otherSprite;
+
+    public override void OnToggle()
+    {
+        base.OnToggle();
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        if (isActivated)
+            spriteRenderer.sprite = otherSprite;
+        else spriteRenderer.sprite = defaultSprite;
+    }
+}
