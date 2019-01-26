a6a41781bc0aa2ac51f5601f1e13ce9a1d07e55b
Unknown <cizl.elias@gmail.com>
2019-01-26 10:21:20 +0100

    Screen controls basis

---
+++ GGJ2019/Assets/Scripts/PanelController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelController : MonoBehaviour
+{
+	ScreenController screenController;
+
+	public void SetScreenController(ScreenController screenController)
+	{
+		this.screenController = screenController;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Return))
+		{
+			screenController.NextScreen();
+		}
+	}
+}
---
+++ GGJ2019/Assets/Scripts/ScreenController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenController : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject[] screens;
+	private int currentScreen;
+
+    void Start()
+    {
+		currentScreen = 0;
+		for (int i = 0; i < screens.Length; i++)
+		{
+			screens[i].SendMessage("SetScreenController", this);
+			screens[i].SetActive(false);
+		}
+
+		screens[currentScreen].SetActive(true);
+    }
+
+	public void NextScreen()
+	{
+		screens[currentScreen].SetActive(false);
+		currentScreen++;
+		if(currentScreen < screens.Length)
+		{
+			screens[currentScreen].SetActive(true);
+		}
+	}
+}
