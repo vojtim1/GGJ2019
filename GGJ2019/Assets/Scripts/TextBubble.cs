c37ad0e68c50f88eaac01916efd0ccd0a3d8d94f
Vojtěch Matouš <vojtim11@seznam.cz>
2019-01-27 12:20:29 +0100

    Main scene + player update

---
+++ GGJ2019/Assets/Scripts/TextBubble.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class TextBubble : MonoBehaviour {
+    public GameObject textBubble;
+
+    public void Say(string text, float time)
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+        text = text.Replace("\\n", "\n");
+        GetComponentInChildren<Text>().text = text;
+        GetComponentInChildren<SelfDestruction>().time = time;
+    }
+}
